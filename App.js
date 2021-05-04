2a11fdef39e1040c8d102e4cff87b12f44102fd6
willywnt <willywinata1688@gmail.com>
2021-05-05 00:56:13 +0700

    rollback

--- App.js
+++ App.js
@@ -1,26 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// import { useFonts } from 'expo-font';
 
 export default function App() {
-  // const [loaded] = useFonts({
-  //   Poppins: require('./assets/fonts/Poppins-Regular.ttf'),
-  // });
-  
-  // if (!loaded) {
-  //   return null;
-  // }
-  
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>Aplikasi Pertama menggunakan Framework React Native</Text>
-        <Text style={styles.title}>Ubah</Text>
-      </View>
-      <View style={styles.body}>
-      </View>
-      <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app!!!!</Text>
     </View>
   );
 }
@@ -30,20 +14,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-  },
-  header:{
-    backgroundColor: '#F88409',
-    paddingLeft: 20,
-    paddingRight: 20,
-    paddingTop: 20,
-    paddingBottom: 20,
-    position:'absolute',
-    top:0,
-  },
-  title:{
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    justifyContent: 'center',
   },
 });
